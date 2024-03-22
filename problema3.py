cc12f899398a947340c110f3cfde7683af30a010
Camille Elgueta <142446228+X-Camille@users.noreply.github.com>
2024-03-22 09:56:43 -0300

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -9,5 +9,22 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+t = tuple((input().split()))
+
+def arbolTrinario(numero):
+    return [numero, [], [], []] # Se define un árbol trinario, considerando  los elementos iguales.
+
+def insertaEnArbolTrinario(arbol, numero): # Función que se carga de insertar el número en el árbol trinario.
+    if arbol == []: # Si el árbol está vacío, se agrega la estructura básica de un árbol trinario.
+        arbol += arbolTrinario(numero)
+    elif numero < arbol[0]: # Si es menor se agrega a la izquierda.
+        insertaEnArbolTrinario(arbol[1], numero)
+    elif numero == arbol[0]: # Si es igual se agrega al centro.
+        insertaEnArbolTrinario(arbol[2], numero)
+    else: # Si es mayor, se agrega a la derecha.
+        insertaEnArbolTrinario(arbol[3], numero)
+
+arbol_trinario = arbolTrinario(int(t[0]))
+for i in t:
+  insertaEnArbolTrinario(arbol_trinario, int(i))
+print(arbol_trinario)
